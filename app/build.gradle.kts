2f7f09ea6302e864825c86fc5ecbbdb0f25bc91a
devstar1224 <dev_sang@naver.com>
2026-02-24 13:38:35 +0900

    Add Firebase and Google Services dependencies, update .gitignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.services)
     id("com.google.android.gms.oss-licenses-plugin")
 }
 
@@ -92,6 +93,10 @@ dependencies {
 
     // WorkManager
     implementation(libs.work.runtime.ktx)
+
+    // Firebase
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
 
     // AdMob
     implementation(libs.google.mobile.ads)
